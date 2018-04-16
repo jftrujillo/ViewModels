9810521dccb99d59fe682dfb9e3cd650865206c7
Jhon Fredy Trujillo Ortega <jhon.trujillo.ortega@gmail.com>
2018-04-16 18:20:19 -0500

    Fix some bugs

--- app/src/main/java/com/rhodar/mobile/codescrum/viewmodel/home/ListFragment.kt
+++ app/src/main/java/com/rhodar/mobile/codescrum/viewmodel/home/ListFragment.kt
@@ -6,12 +6,9 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.Unbinder
 import com.rhodar.mobile.codescrum.viewmodel.R
